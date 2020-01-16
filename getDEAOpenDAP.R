9768f0e345cf54d365ad7b815f09261244ae14c4
RossDSearle <ross.searle@csiro.au>
2020-01-16 19:25:19 +1000

    Sync

--- getDEAOpenDAP.R
+++ getDEAOpenDAP.R
@@ -8,6 +8,56 @@ library(htmltidy)
 library(async)
 
 
+# NCI Paths
+# NCI THredds Root - http://dap.nci.org.au/thredds/catalog.html
+# GA DataCube - http://dap.nci.org.au/thredds/remoteCatalogService?catalog=http://dapds00.nci.org.au/thredds/catalogs/fk4/catalog.xml
+
+
+# Direct Download from within R Raster
+
+rootOut <- 'c:/temp/fcverDirect'
+xv <- seq(-19, 21, 1)
+yv <- seq(-10, -43, -1)
+#att <- 'PV_PC_50'
+att <- 'NPV_PC_50'
+rootOut <- 'c:/temp/fcver'
+
+
+for (i in 1:length(xv)) {
+   for (j in 1:length(yv)) {
+      
+      linkoURL <- paste0('http://dap.nci.org.au/thredds/remoteCatalogService?catalog=http://dapds00.nci.org.au/thredds/catalog/fk4/datacube/002/FC/FC-percentile/ANNUAL/', xv[i], '_', yv[j], '/catalog.xml')
+      doc <- htmlParse(linkoURL)
+      links <- xpathSApply(doc, '//a[@href]', xmlValue)
+      linksf <- links[grepl('.nc$',links)]
+      
+      if(length(linksf) > 0){
+         outd <- paste0(rootOut,'/', att, '/x_', xv[i], '_y_', yv[j] )
+         if(!dir.exists(outd)){dir.create(outd, recursive = T)}
+         
+         #tInf <-  getTileInfo(xv[i], yv[j], linksf[1])
+         
+         for (k in 1:length(linksf)) {
+            fname <- linksf[k]
+            outPath <- str_replace(paste0(outd, '/', att, '_', fname), '.nc', '.tif')
+            if(!file.exists(outPath)){
+               
+               print(fname)
+               url <- paste0('http://dapds00.nci.org.au/thredds/dodsC/fk4/datacube/002/FC/FC-percentile/ANNUAL/', xv[i], '_', yv[j], '/', fname)
+               r <- raster(url, varname = att, band = 1)
+               
+               #try(  downloadOpenDAP2(url, tInf),  silent=F)
+               
+               writeRaster(r, outPath, datatype='INT2S')
+            }
+         }
+      }
+   }
+}
+
+
+
+
 # rb <- brick('E:/temp/fract/x_0_y_-14/LS_FC_PC_3577_0_-14_19870101.nc', varname='BS_PC_50')
 # rl <- raster('E:/temp/fract/x_0_y_-14/LS_FC_PC_3577_0_-14_19870101.nc', varname='BS_PC_50')
 # plot(rl)
@@ -42,10 +92,7 @@ library(async)
  
 
  
- xv <- seq(-19, 21, 1)
- yv <- seq(-10, -43, -1)
- att <- 'PV_PC_50'
- rootOut <- 'c:/temp/fcver'
+
  
  for (i in 1:length(xv)) {
    for (j in 1:length(yv)) {
@@ -151,43 +198,4 @@ library(async)
  
  
  
- # Direct Download from within R Raster
  
- rootOut <- 'c:/temp/fcverDirect'
- 
- 
- for (i in 1:length(xv)) {
-   for (j in 1:length(yv)) {
-     
-     linkoURL <- paste0('http://dap.nci.org.au/thredds/remoteCatalogService?catalog=http://dapds00.nci.org.au/thredds/catalog/fk4/datacube/002/FC/FC-percentile/ANNUAL/', xv[i], '_', yv[j], '/catalog.xml')
-     doc <- htmlParse(linkoURL)
-     links <- xpathSApply(doc, '//a[@href]', xmlValue)
-     linksf <- links[grepl('.nc$',links)]
-     
-     if(length(linksf) > 0){
-       outd <- paste0(rootOut,'/', att, '/x_', xv[i], '_y_', yv[j] )
-       if(!dir.exists(outd)){dir.create(outd, recursive = T)}
-       
-       #tInf <-  getTileInfo(xv[i], yv[j], linksf[1])
-       
-       for (k in 1:length(linksf)) {
-         fname <- linksf[k]
-         outPath <- str_replace(paste0(outd, '/', fname), '.nc', '.tif')
-         if(!file.exists(outPath)){
-           
-           print(fname)
-           url <- paste0('http://dapds00.nci.org.au/thredds/dodsC/fk4/datacube/002/FC/FC-percentile/ANNUAL/', xv[i], '_', yv[j], '/', fname)
-           r <- raster(url, varname = "PV_PC_50", band = 1)
-           
-           #try(  downloadOpenDAP2(url, tInf),  silent=F)
-           
-           writeRaster(r, outPath, datatype='INT2S')
-         }
-       }
-     }
-   }
- }
- 
-
-
-
